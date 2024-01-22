837fd45337b3ed0d272c468f31cf14bbedcc1de6
qkrdmsql <kelly030813@kaist.ac.kr>
2024-01-22 22:05:43 +0900

    get info from api

--- routes/elec.js
+++ routes/elec.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+var express = require('express');
+var router = express.Router();
 
 const url = "https://api.odcloud.kr/api/EvInfoServiceV2/v1/getEvSearchList"
 require('dotenv').config();
@@ -7,14 +9,17 @@ const addrSetting = '[addr::LIKE]'
 const addrValue = '대전광역시 유성구'
 const encodedValue = encodeURIComponent(addrSetting)+'='+encodeURIComponent(addrValue)
 
-axios.get(`${url}?page=1&perPage=10&cond${encodedValue}&serviceKey=${SERVICE_KEY}`)
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('에러 발생:', error.message);
-    if (error.response) {
-        console.error('에러 응답 데이터:', error.response.data);
-      }
+  router.get('/', function(req, res, next) {
+    axios.get(`${url}?page=1&perPage=10&cond${encodedValue}&serviceKey=${SERVICE_KEY}`)
+    .then(response => {
+      res.send(response.data.data);
+    })
+    .catch(error => {
+      console.error('에러 발생:', error.message);
+      if (error.response) {
+          console.error('에러 응답 데이터:', error.response.data);
+        }
+    });
   });
-
+  
+  module.exports = router;
